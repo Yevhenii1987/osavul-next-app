430534849b4b4afb37a6fa5c7965940663492e1f
YevheniiBilousov <yevhenii.bilousov@gotoinc.co>
2025-07-26 18:52:47 +0300

    fixed media queries

--- components/Sections/FeaturesSection.jsx
+++ components/Sections/FeaturesSection.jsx
@@ -7,24 +7,26 @@ export default function FeaturesSection({ features }) {
   return (
     <section className="section product-features">
       <div className="container">
-        {features.map((item, index) => (
-          <motion.div
-            key={index}
-            className="product-feature"
-            initial={{ opacity: 0, scale: 0.5 }}
-            whileInView={{ opacity: 1, scale: 1 }}
-            viewport={{ once: true, margin: "-200px 0px 0px 0px" }}
-            transition={{ type: 'spring', delay: 0.1, duration: 0.3 }}
-          >
-            <div className="product-feature-top">
-              <div className="product-feature-icon">
-                <Image src={item.icon} alt=" " />
+        <div className="product-features-grid">
+          {features.map((item, index) => (
+            <motion.div
+              key={index}
+              className="product-feature"
+              initial={{ opacity: 0, scale: 0.5 }}
+              whileInView={{ opacity: 1, scale: 1 }}
+              viewport={{ once: true, margin: "-200px 0px 0px 0px" }}
+              transition={{ type: 'spring', delay: 0.1, duration: 0.3 }}
+            >
+              <div className="product-feature-top">
+                <div className="product-feature-icon">
+                  <Image src={item.icon} alt=" " />
+                </div>
+                <h4 className="product-feature-title">{item.title}</h4>
               </div>
-              <h4 className="product-feature-title">{item.title}</h4>
-            </div>
-            <p className="product-feature-text">{item.text}</p>
-          </motion.div>
-        ))}
+              <p className="product-feature-text">{item.text}</p>
+            </motion.div>
+          ))}
+        </div>
       </div>
     </section>
   );
