d081b3c33614a32aa7dadc596f2f645f9e585023
YevheniiBilousov <yevhenii.bilousov@gotoinc.co>
2025-07-27 17:31:29 +0300

    canged fonts download

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,5 @@
+import { Inter, IBM_Plex_Sans } from 'next/font/google';
+
 import MainHeader from '@/components/Header';
 import MainFooter from '@/components/Footer';
 import './globals.css';
@@ -8,9 +10,23 @@ export const metadata = {
     'An advanced AI-driven platform for information assessment and narrative impact analysis.',
 };
 
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-inter',
+  display: 'swap',
+})
+
+const IBM_plex_sans = IBM_Plex_Sans({
+  subsets: ['latin'],
+  weight: ['400', '600', '700'],
+  variable: '--font-ibm-plex-sans',
+  display: 'swap',
+})
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${inter.variable} ${IBM_plex_sans.variable}`}>
       <body>
         <div id="modal"></div>
         <MainHeader />
